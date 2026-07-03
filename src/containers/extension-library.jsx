384ba01a06270b219dbc1a23330875a1dd4e4e78
SYSTEM-RAM-OP <mmmh74mmmh@gmail.com>
2026-07-03 10:53:34 +0800

    上传完整代码

---
+++ src/containers/extension-library.jsx
@@ -0,0 +1,242 @@
+import bindAll from 'lodash.bindall';
+import PropTypes from 'prop-types';
+import React from 'react';
+import VM from 'scratch-vm';
+import {defineMessages, injectIntl, intlShape} from 'react-intl';
+import log from '../lib/log';
+
+import extensionLibraryContent, {
+    galleryError,
+    galleryLoading,
+    galleryMore
+} from '../lib/libraries/extensions/index.jsx';
+import extensionTags from '../lib/libraries/tw-extension-tags';
+
+import LibraryComponent from '../components/library/library.jsx';
+import extensionIcon from '../components/action-menu/icon--sprite.svg';
+
+const messages = defineMessages({
+    extensionTitle: {
+        defaultMessage: 'Choose an Extension',
+        description: 'Heading for the extension library',
+        id: 'gui.extensionLibrary.chooseAnExtension'
+    }
+});
+
+const toLibraryItem = extension => {
+    if (typeof extension === 'object') {
+        return ({
+            rawURL: extension.iconURL || extensionIcon,
+            ...extension
+        });
+    }
+    return extension;
+};
+
+const translateGalleryItem = (extension, locale) => ({
+    ...extension,
+    name: extension.nameTranslations[locale] || extension.name,
+    description: extension.descriptionTranslations[locale] || extension.description
+});
+
+let cachedGallery = null;
+
+const fetchLibrary = async () => {
+    const res = await fetch('https://extensions.turbowarp.org/generated-metadata/extensions-v0.json');
+    if (!res.ok) {
+        throw new Error(`HTTP status ${res.status}`);
+    }
+    const data = await res.json();
+    return data.extensions.map(extension => ({
+        name: extension.name,
+        nameTranslations: extension.nameTranslations || {},
+        description: extension.description,
+        descriptionTranslations: extension.descriptionTranslations || {},
+        extensionId: extension.id,
+        extensionURL: `https://extensions.turbowarp.org/${extension.slug}.js`,
+        iconURL: `https://extensions.turbowarp.org/${extension.image || 'images/unknown.svg'}`,
+        tags: ['tw'],
+        credits: [
+            ...(extension.original || []),
+            ...(extension.by || [])
+        ].map(credit => {
+            if (credit.link) {
+                return (
+                    <a
+                        href={credit.link}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={credit.name}
+                    >
+                        {credit.name}
+                    </a>
+                );
+            }
+            return credit.name;
+        }),
+        docsURI: extension.docs ? `https://extensions.turbowarp.org/${extension.slug}` : null,
+        samples: extension.samples ? extension.samples.map(sample => ({
+            href: `${process.env.ROOT}editor?project_url=https://extensions.turbowarp.org/samples/${encodeURIComponent(sample)}.sb3`,
+            text: sample
+        })) : null,
+        incompatibleWithScratch: !extension.scratchCompatible,
+        featured: true
+    }));
+};
+
+const fetchMyLibrary = async () => {
+    const res = await fetch('https://SYSTEM-RAM-OP.github.io/extensions-v0.json');
+    
+    if (!res.ok) {
+        throw new Error(`HTTP status ${res.status}`);
+    }
+    const data = await res.json();
+    
+    return data.extensions.map(extension => ({
+        ...extension,
+        extensionURL: `https://SYSTEM-RAM-OP.github.io/extensions/${extension.slug}.js`,
+        tags: ['be'],
+        featured: true
+    }));
+};
+
+// ==========================================================
+
+class ExtensionLibrary extends React.PureComponent {
+    constructor (props) {
+        super(props);
+        bindAll(this, [
+            'handleItemSelect',
+            'fetchRemoteExtensions'
+        ]);
+        this.state = {
+            gallery: cachedGallery,
+            galleryError: null,
+            galleryTimedOut: false,
+            remoteExtensions: []
+        };
+    }
+
+    componentDidMount () {
+        this.fetchRemoteExtensions();
+
+        if (!this.state.gallery) {
+            const timeout = setTimeout(() => {
+                this.setState({
+                    galleryTimedOut: true
+                });
+            }, 750);
+
+            fetchLibrary()
+                .then(gallery => {
+                    cachedGallery = gallery;
+                    this.setState({
+                        gallery
+                    });
+                    clearTimeout(timeout);
+                })
+                .catch(error => {
+                    log.error(error);
+                    this.setState({
+                        galleryError: error
+                    });
+                    clearTimeout(timeout);
+                });
+        }
+    }
+
+    fetchRemoteExtensions = async () => {
+        try {
+            const data = await fetchMyLibrary();
+            this.setState({ remoteExtensions: data });
+        } catch (err) {
+            log.error('加载自定义远程扩展失败', err);
+        }
+    }
+
+    handleItemSelect (item) {
+        if (item.href) { return; }
+
+        const extensionId = item.extensionId;
+
+        if (extensionId === 'custom_extension') {
+            this.props.onOpenCustomExtensionModal();
+            return;
+        }
+
+        if (extensionId === 'procedures_enable_return') {
+            this.props.onEnableProcedureReturns();
+            this.props.onCategorySelected('myBlocks');
+            return;
+        }
+
+        const url = item.extensionURL ? item.extensionURL : extensionId;
+        if (!item.disabled) {
+            if (this.props.vm.extensionManager.isExtensionLoaded(extensionId)) {
+                this.props.onCategorySelected(extensionId);
+            } else {
+                this.props.vm.extensionManager.loadExtensionURL(url)
+                    .then(() => {
+                        this.props.onCategorySelected(extensionId);
+                    })
+                    .catch(err => {
+                        log.error(err);
+                        alert(err);
+                    });
+            }
+        }
+    }
+
+    render () {
+        let library = null;
+        if (this.state.gallery || this.state.galleryError || this.state.galleryTimedOut) {
+            library = extensionLibraryContent.map(toLibraryItem);
+            library.push('---');
+            
+            if (this.state.gallery) {
+                library.push(toLibraryItem(galleryMore));
+                const locale = this.props.intl.locale;
+                library.push(
+                    ...this.state.gallery
+                        .filter(i => i.extensionId !== 'faceSensing')
+                        .map(i => translateGalleryItem(i, locale))
+                        .map(toLibraryItem)
+                );
+            } else if (this.state.galleryError) {
+                library.push(toLibraryItem(galleryError));
+            } else {
+                library.push(toLibraryItem(galleryLoading));
+            }
+
+            if (this.state.remoteExtensions && this.state.remoteExtensions.length > 0) {
+                library.push(...this.state.remoteExtensions.map(toLibraryItem));
+            }
+        }
+
+        return (
+            <LibraryComponent
+                data={library}
+                filterable
+                persistableKey="extensionId"
+                id="extensionLibrary"
+                tags={extensionTags}
+                title={this.props.intl.formatMessage(messages.extensionTitle)}
+                visible={this.props.visible}
+                onItemSelected={this.handleItemSelect}
+                onRequestClose={this.props.onRequestClose}
+            />
+        );
+    }
+}
+
+ExtensionLibrary.propTypes = {
+    intl: intlShape.isRequired,
+    onCategorySelected: PropTypes.func,
+    onEnableProcedureReturns: PropTypes.func,
+    onOpenCustomExtensionModal: PropTypes.func,
+    onRequestClose: PropTypes.func,
+    visible: PropTypes.bool,
+    vm: PropTypes.instanceOf(VM).isRequired
+};
+
+export default injectIntl(ExtensionLibrary);
